60d87e906fe448f9c02dc6f45ccd877b57123830
Per4ukkk <74988014+Per4ukkk@users.noreply.github.com>
2020-11-29 10:08:04 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ module.exports = {
     url : "mongodb+srv://perecc:per4uHka@cluster0.vsah3.mongodb.net/Cluster0?retryWrites=true&w=majority"
 };
 
-//per4uHka
-//Cluster0
+//password:per4uHka
+//dbname:Cluster0
