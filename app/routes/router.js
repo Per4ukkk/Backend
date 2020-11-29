1b5b94e20a08642b354537777c8c8c8e585d8410
Per4ukkk <74988014+Per4ukkk@users.noreply.github.com>
2020-11-29 10:11:12 +0500

    Update router.js

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,9 +2,7 @@ const nanoid = require('nanoid')
 const router = require('./router');
 module.exports = function(app, db) {
     router(app, db);
-    // Тут, позже, будут и другие обработчики маршрутов
 };
-// note_routes.js
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, database) {
 
@@ -45,44 +43,14 @@ module.exports = function(app, database) {
         });
     });
     app.get('/:url/views', (req, res) => {
-       //принять ссылку
-        //возвратить значение из базы данных по данной ссылке
         const shortenedUrl = req.params.url;
         const query ={shortenedUrl:shortenedUrl}
-
         db.collection('urlBindings').findOne(query, (err, item) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
                 let response = {viewCount: item.count}
                 res.send(response,200)
-            }
-
-        });
-
-    });
-    app.put ('/notes/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        const note = { text: req.body.body, title: req.body.title };
-        db.collection('notes').update(details, note, (err, result) => {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                res.send(note);
-            }
-        });
-    });
-
-    app.get('/notes/:id', (req, res) => {
-
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('notes').findOne(details, (err, item) => {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                res.send(item);
             }
         });
     });
